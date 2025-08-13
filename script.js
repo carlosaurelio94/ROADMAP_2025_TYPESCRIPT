8eae39c6cc8e071a8f4184f0af13dd561d346f5b
Carlos Rodriguez <carlosarc10@gmail.com>
2025-08-12 21:29:05 -0300

    pocos console log

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 console.log("ESTOY TARABAHA");
 console.log("PARA COMPILAR USAR TSC. EJEMPLO TSC script.ts. ESTO GENERA UN JS");
 console.log("PARA COMPILAR EN MODO OBSERVADOR USAR TSC -w. EJEMPLO TSC -w script.ts. ESTO GENERA UN JS");
 console.log("HOA");
+let estudiasteJavaScript = true;
+if (estudiasteJavaScript) {
+    console.log("SIGO");
+}
+else {
+    console.log("NO SIGAS");
+}
+export {};
 //# sourceMappingURL=script.js.map
--- script2.js
+++ script2.js
@@ -1,4 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 console.log("EJERCICIOS DE PRUEBA");
+export {};
 //# sourceMappingURL=script2.js.map
